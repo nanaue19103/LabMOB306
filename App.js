239d25331928dfd66d2fea8db8de2e787cc40bda
PC <hungtvph27819@fpt.edu.vn>
2023-01-30 10:48:54 +0700

    Add new project LAB

--- App.js
+++ App.js
@@ -1,17 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View,TextInput,FlatList,Button,ListView } from 'react-native';
 
 export default function App() {
+  const [name,setName] = useState('');
+  const [id,setId] = useState('');
+  const [data,setData] = useState([]);
+  const addToList =()=>{
+    setData([...data,{name,id}]);
+    setName('');
+    setId('');
+  };
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput 
+      placeholder='Name: '
+      value={name}
+      onChangeText={Text => setName(Text)}
+      >       
+      </TextInput>
+      <TextInput 
+      placeholder='Id: '
+      value={id}
+      onChangeText={Text => setId(Text)}
+      >       
+      </TextInput>
+      <Button
+      title='btn'
+      onPress={addToList}
+      >
+
+      </Button>
+      <FlatList
+      data={data}
+      renderItem={({item})=>(
+        <View>
+          <Text>Name :{item.name}</Text>
+          <Text>Id :{item.id}</Text>
+        </View>
+      )}
+      KeyE={(item, index)=>index.toString()}
+      >
+
+      </FlatList>
     </View>
+    
+   
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    margin: 50,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
